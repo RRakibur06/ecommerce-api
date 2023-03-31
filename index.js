c4bca7b382a6a1a372c3ea0d2193f9983a0d84dc
RRakibur06 <rahmanrakibur06@gmail.com>
2023-04-01 01:13:01 +0600

    second commit

--- index.js
+++ index.js
@@ -7,10 +7,12 @@ const productRoute = require("./routes/product");
 const cartRoute = require("./routes/cart");
 const orderRoute = require("./routes/order");
 const stripeRoute = require("./routes/stripe");
+const cors = require("cors");
 dotenv.config();
 
 mongoose.connect(process.env.MONGO_URL).then(()=>console.log("DB connection succesfull."));
 
+app.use(cors());
 app.use(express.json());
 app.use("/api/users", userRoute);
 app.use("/api/products", productRoute);
